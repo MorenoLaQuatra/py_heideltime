522c6394547c9694d39d47e82a28ee22bfb8a70b
MorenoLQ <moreno.laquatra@gmail.com>
2022-01-13 18:38:20 +0100

    Update py_heideltime_tilse.py

--- py_heideltime/py_heideltime_tilse.py
+++ py_heideltime/py_heideltime_tilse.py
@@ -97,14 +97,14 @@ def exec_java_heideltime(filename, path, language, document_type, document_creat
         extractor_start_time = time.time()
         if document_creation_time == 'yyyy-mm-dd':
             #java_command = 'java -jar ' + path + '/Heideltime/de.unihd.dbs.heideltime.standalone.jar ' + document_type + ' -l ' + language + ' -c ' + config_props_path + ' ' + filename
-            java_command = "java -Dfile.encoding=UTF-8 -jar " + path + "/Heideltime/tilse/de.unihd.dbs.heideltime.standalone.jar " + config_props_path + " " + filename
+            java_command = "java -jar " + path + "/Heideltime/tilse/de.unihd.dbs.heideltime.standalone.jar " + config_props_path + " " + filename
         else:
             '''
             java_command = 'java -jar ' + path + '/Heideltime/de.unihd.dbs.heideltime.standalone.jar  -dct ' + \
                                document_creation_time + ' -t ' + document_type + ' -l ' + language + ' -c ' + config_props_path + ' ' + filename
             '''
             #java_command = "java -Dfile.encoding=UTF-8 -jar " + path + "/Heideltime/tilse/de.unihd.dbs.heideltime.standalone.jar " + filename + " -l " + language.upper() + " -dct " + str(document_creation_time) + " -t document_type " + " -c " + config_props_path
-            java_command = "java -Dfile.encoding=UTF-8 -jar " + path + "/Heideltime/tilse/de.unihd.dbs.heideltime.standalone.jar " + config_props_path + " " + filename
+            java_command = "java -jar " + path + "/Heideltime/tilse/de.unihd.dbs.heideltime.standalone.jar " + config_props_path + " " + filename
             # run java heideltime standalone version to get all dates
 
         # TimeML text from java output
